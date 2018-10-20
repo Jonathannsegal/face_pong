3e0650fef89abc1f556f730b2630ca9cf970bf4d
bdcohen1 <bdcohen@iastate.edu>
2018-10-19 21:30:11 -0500

    add

--- js/main.js
+++ js/main.js
@@ -10,13 +10,40 @@ var engine = Engine.create();
 // create a renderer
 var render = Render.create({
     element: document.body,
-    engine: engine
+    engine: engine,
+    options: {
+        width: screen.width,
+        height: window.innerHeight,
+        pixelRatio: 1,
+        background: 'transparent',
+        wireframeBackground: '#222',
+        hasBounds: false,
+        enabled: true,
+        wireframes: false,
+        showSleeping: true,
+        showDebug: false,
+        showBroadphase: false,
+        showBounds: false,
+        showVelocity: false,
+        showCollisions: false,
+        showSeparations: false,
+        showAxes: false,
+        showPositions: false,
+        showAngleIndicator: false,
+        showIds: false,
+        showShadows: false,
+        showVertexNumbers: false,
+        showConvexHulls: false,
+        showInternalEdges: false,
+        showMousePosition: false
+    }
+
 });
 
 // create two boxes and a ground
 var boxA = Bodies.rectangle(400, 200, 80, 80);
 var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var ground = Bodies.rectangle(400, 610, screen.width, 60, { isStatic: true });
 
 // add all of the bodies to the world
 World.add(engine.world, [boxA, boxB, ground]);

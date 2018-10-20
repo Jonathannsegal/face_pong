2b590e8f508a19b9a53d52b367e0d1e73e79bfa6
Jonathan <41304994+Jonathannsegal@users.noreply.github.com>
2018-10-20 15:35:57 -0500

    asda

--- js/main1.js
+++ js/main1.js
@@ -107,7 +107,7 @@ function draw(){
 
 
 
-  if(shapes.length <= 9){
+  if(shapes.length <= 49){
     var newBox = Bodies.rectangle(Common.random(220,render.canvas.width-40),0,80,80)
     /*var newBox = Bodies.rectangle(Common.random(0,1000),200,80,80,{
       friction: 0,

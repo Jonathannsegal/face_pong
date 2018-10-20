db4ef17156b6604d84baaee47ca0451682fd522c
bdcohen1 <bdcohen@iastate.edu>
2018-10-19 22:02:22 -0500

    add

---
+++ js/main1.js
@@ -0,0 +1,55 @@
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+// create an engine
+var engine = Engine.create();
+
+// create a renderer
+var render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: screen.width,
+        height: window.innerHeight,
+        pixelRatio: 1,
+        background: 'transparent',
+        wireframeBackground: '#222',
+        hasBounds: false,
+        enabled: true,
+        wireframes: false,
+        showSleeping: true,
+        showDebug: false,
+        showBroadphase: false,
+        showBounds: false,
+        showVelocity: false,
+        showCollisions: false,
+        showSeparations: false,
+        showAxes: false,
+        showPositions: true,
+        showAngleIndicator: false,
+        showIds: false,
+        showShadows: false,
+        showVertexNumbers: false,
+        showConvexHulls: false,
+        showInternalEdges: false,
+        showMousePosition: false
+    }
+
+});
+
+// create two boxes and a ground
+var boxA = Bodies.rectangle(40, 200, 80, 80);
+var boxB = Bodies.rectangle(screen.width-40, 50, 80, 80);
+var ground = Bodies.rectangle(screen.width/2, 610, screen.width, 60, { isStatic: true });
+
+// add all of the bodies to the world
+World.add(engine.world, [boxA, boxB, ground]);
+
+// run the engine
+Engine.run(engine);
+
+// run the renderer
+Render.run(render);

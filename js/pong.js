df25729eae864bb55385b2773e38326a5e28f52d
Jonathan <41304994+Jonathannsegal@users.noreply.github.com>
2018-10-20 23:58:49 -0500

    update

--- js/pong.js
+++ js/pong.js
@@ -155,11 +155,11 @@ Events.on(engine, "collisionStart", function(event){
 function checkOver(){
   if(scorePlayer1 >= 5){
     localStorage.setItem("winner","Player 1 Wins");
-    window.location.href = 'GameOverScreen2.html';
+    window.location.href = 'https://jonathansegal.info/hungryhungryhacker/GameOverScreen2.html';
   }
   if (scorePlayer2 >= 5){
     localStorage.setItem("winner","Player 2 Wins");
-    window.location.href = 'GameOverScreen2.html';
+    window.location.href = 'https://jonathansegal.info/hungryhungryhacker/GameOverScreen2.html';
   }
 }
 
